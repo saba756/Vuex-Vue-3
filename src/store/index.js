9c729ee1a027a51b12073d9053bc8688b02bb1dc
Saba Tahir <sata@ciklum.com>
2022-01-06 14:54:37 +0500

    implemented vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,44 @@
-import { createStore } from 'vuex'
-
+import { createStore } from "vuex";
+import axios from "axios";
 export default createStore({
   state: {
+    counter: 0,
+    colorCode: "blue",
   },
   mutations: {
+    increaseCounter(state, RandomNumber) {
+      state.counter += RandomNumber;
+    },
+    decreaseCounter(state, RandomNumber) {
+      state.counter -= RandomNumber;
+    },
+    setColorCode(state, newValue) {
+      state.colorCode = newValue;
+    },
   },
   actions: {
+    increaseCounter({ commit }) {
+      axios(
+        "https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new"
+      ).then((response) => {
+        commit("increaseCounter", response.data);
+      });
+    },
+    decreaseCounter({ commit }) {
+      axios(
+        "https://www.random.org/integers/?num=1&min=1&max=6&col=1&base=10&format=plain&rnd=new"
+      ).then((response) => {
+        commit("decreaseCounter", response.data);
+      });
+    },
+    setColorCode({ commit }, newValue) {
+      commit("setColorCode", newValue);
+    },
   },
-  modules: {
-  }
-})
+  getters: {
+    counterSquared(state) {
+      return state.counter * state.counter;
+    },
+  },
+  modules: {},
+});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Counter />
+    <CounterSquared />
+    <Buttons />
+    <div>
+      <input v-model="colorCode" type="text" placeholder="type color code" />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Counter from "../components/Counter.vue";
+import CounterSquared from "../components/CounterSquare.vue";
+import Buttons from "../components/Buttons.vue";
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    Counter,
+    CounterSquared,
+    Buttons,
+  },
+  computed: {
+    colorCode: {
+      get() {
+        return this.$store.state.colorCode;
+      },
+      set(newValue) {
+        return this.$store.dispatch("setColorCode", newValue);
+      },
+    },
+  },
+};
 </script>
+<style>
+div {
+  margin-bottom: 10px;
+}
+.counter {
+  font-size: 80px;
+}
+.buttons button {
+  font-size: 40px;
+  width: 100px;
+  margin: 0px 10px;
+}
+</style>
